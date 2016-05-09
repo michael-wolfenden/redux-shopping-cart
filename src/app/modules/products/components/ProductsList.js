0084e96783008ca7fb2d0998f232f87e5101bf50
Michael Wolfenden <michael.wolfenden@gmail.com>
2016-05-09 10:25:36 +1000

    Remove unneeded brackets

--- src/app/modules/products/components/ProductsList.js
+++ src/app/modules/products/components/ProductsList.js
@@ -1,12 +1,11 @@
 import React, { PropTypes } from 'react'
 const { node, string } = PropTypes
 
-const ProductsList = ({ title, children }) => (
+const ProductsList = ({ title, children }) =>
     <div>
         <h3>{title}</h3>
         <div>{children}</div>
     </div>
-)
 
 ProductsList.propTypes = {
     children: node,
